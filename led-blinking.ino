060a02b3c1f93748f5b4e2819e7945098d724dad
Muhammad Hashir Bin Khalid <mohammadhashirbinkhalid@gmail.com>
2019-09-13 12:26:10 +0500

    Produced error in file

--- led-blinking.ino
+++ led-blinking.ino
@@ -1,27 +1,18 @@
-/*
-  Code by: www.munphurid.com
-  M.Hashir
-
-  This is a code that blinks LED on pin 13 of Arduino
-
-  Hardware:
-  -Arduino
-
-*/
-
 int led = 13;    //LED is connected to pin 13 of Arduino
+int buzzzer = 14  /Buzzer is connectred to pin 14
 
-void setup() {
+void setup() 
   pinMode(led, OUTPUT);     //Do not edit this line
+  pinMode(Buzzer , OUTPUT); //Do not edit this line
 }
 
 void loop() {
   digitalWrite(led, HIGH);    //Turn on led
-
+  digitalWrite(buzzer, LOW); //Turn off buzzer
   delay(3000);                  //wait
 
   digitalWrite(led, LOW);     //Turn off led
-
+  digitalWrite(buzzzer, HIGH); //Turn on the buzzer
   delay(4000);                  //wait
 
   //now go to the start of void loop()
